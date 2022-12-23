d4253a712c656639f6dbd53c2d0a3fb35a140d3c
DİYAR DEMİR <99801830+diyardemir47@users.noreply.github.com>
2022-12-23 20:38:40 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿
+namespace MatematikHesaplamalari
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.hesapla = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dikdortgenhesapla = new System.Windows.Forms.Button();
+            this.ucgenhesaplama = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(269, 129);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.textBox1.ForeColor = System.Drawing.SystemColors.Menu;
+            this.textBox1.Location = new System.Drawing.Point(240, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("OCR A Extended", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(21, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(160, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "BİR KENARI GİRİN:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "ALAN=";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(105, 104);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "0";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(103, 134);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(16, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(23, 134);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 17);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "ÇEVRE=";
+            // 
+            // hesapla
+            // 
+            this.hesapla.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.hesapla.Font = new System.Drawing.Font("OCR A Extended", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hesapla.Location = new System.Drawing.Point(383, 45);
+            this.hesapla.Name = "hesapla";
+            this.hesapla.Size = new System.Drawing.Size(103, 34);
+            this.hesapla.TabIndex = 10;
+            this.hesapla.Text = "HESAPLA";
+            this.hesapla.UseVisualStyleBackColor = false;
+            this.hesapla.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("OCR A Extended", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(23, 69);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(176, 17);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "UZUN KENARI GİRİN:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.textBox2.ForeColor = System.Drawing.SystemColors.Menu;
+            this.textBox2.Location = new System.Drawing.Point(240, 67);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 11;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.ucgenhesaplama);
+            this.panel1.Controls.Add(this.dikdortgenhesapla);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.hesapla);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Location = new System.Drawing.Point(26, 149);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(540, 179);
+            this.panel1.TabIndex = 13;
+            // 
+            // dikdortgenhesapla
+            // 
+            this.dikdortgenhesapla.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.dikdortgenhesapla.Font = new System.Drawing.Font("OCR A Extended", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dikdortgenhesapla.Location = new System.Drawing.Point(383, 45);
+            this.dikdortgenhesapla.Name = "dikdortgenhesapla";
+            this.dikdortgenhesapla.Size = new System.Drawing.Size(103, 34);
+            this.dikdortgenhesapla.TabIndex = 13;
+            this.dikdortgenhesapla.Text = "HESAPLA";
+            this.dikdortgenhesapla.UseVisualStyleBackColor = false;
+            this.dikdortgenhesapla.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // ucgenhesaplama
+            // 
+            this.ucgenhesaplama.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.ucgenhesaplama.BackgroundImage = global::MatematikHesaplamalari.Properties.Resources.rounded_rectangle1;
+            this.ucgenhesaplama.Font = new System.Drawing.Font("OCR A Extended", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ucgenhesaplama.Location = new System.Drawing.Point(383, 45);
+            this.ucgenhesaplama.Name = "ucgenhesaplama";
+            this.ucgenhesaplama.Size = new System.Drawing.Size(103, 34);
+            this.ucgenhesaplama.TabIndex = 14;
+            this.ucgenhesaplama.Text = "HESAPLA";
+            this.ucgenhesaplama.UseVisualStyleBackColor = false;
+            this.ucgenhesaplama.Click += new System.EventHandler(this.ucgenhesaplama_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::MatematikHesaplamalari.Properties.Resources.rounded_rectangle1;
+            this.pictureBox3.Location = new System.Drawing.Point(229, 48);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(123, 78);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 16;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::MatematikHesaplamalari.Properties.Resources.square1;
+            this.pictureBox2.Location = new System.Drawing.Point(132, 51);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(74, 65);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 15;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::MatematikHesaplamalari.Properties.Resources.triangle1;
+            this.pictureBox1.Location = new System.Drawing.Point(376, 48);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(84, 78);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("MV Boli", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(240, 349);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(155, 31);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "DİYARİDEM";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Goldenrod;
+            this.ClientSize = new System.Drawing.Size(623, 389);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "MATEMATİK HESAPLAMALARI";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button hesapla;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button dikdortgenhesapla;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button ucgenhesaplama;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MatematikHesaplamalari
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int birkenar;
+            birkenar = Convert.ToInt32(textBox1.Text);
+            int alan;
+            alan = birkenar * birkenar;
+            int cevre;
+            cevre = birkenar + birkenar + birkenar + birkenar;
+            label4.Text = alan.ToString();
+            label5.Text = cevre.ToString();
+            label3.Visible = true;
+            label4.Visible = true;
+            label5.Visible = true;
+            label6.Visible = true;
+
+           
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label7.Visible = false;
+            textBox2.Visible = false;
+            panel1.Visible = true;
+            label3.Visible = false;
+            label4.Visible = false;
+            label5.Visible = false;
+            label6.Visible = false;
+            label1.Text = "KARE";
+         dikdortgenhesapla.Visible = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label7.Visible = true;
+            textBox2.Visible = true;
+            panel1.Visible = true;
+            label3.Visible = false;
+            label4.Visible = false;
+            label5.Visible = false;
+            label6.Visible = false;
+            label1.Text = "DİKDÖRTGEN";
+            label2.Text = "KISA KENARI GİRİN:";
+            hesapla.Visible = false;
+            dikdortgenhesapla.Visible = true;
+
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            panel1.Visible = false;
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            label3.Visible = true;
+            label4.Visible = true;
+            label5.Visible = true;
+            label6.Visible = true;
+
+            int kisakenar, uzunkenar, aland, cevred;
+            kisakenar = Convert.ToInt32(textBox1.Text);
+            uzunkenar = Convert.ToInt32(textBox2.Text);
+
+            aland = uzunkenar * kisakenar;
+            cevred = 2 * (uzunkenar + kisakenar);
+
+            label4.Text = aland.ToString();
+            label5.Text = cevred.ToString();
+
+         
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            label7.Visible = false;
+            textBox2.Visible = false;
+            panel1.Visible = true;
+            label3.Visible = false;
+            label4.Visible = false;
+            label5.Visible = false;
+            label6.Visible = false;
+            label1.Text = "ÜÇGEN";
+            label2.Text = "YÜKSEKLİĞİ GİRİNİZ:";
+            label7.Text = "TABANI GİRİNİZ:";
+            dikdortgenhesapla.Visible = false;
+            hesapla.Visible = false;
+            ucgenhesaplama.Visible = true;
+            label7.Visible = true;
+            textBox2.Visible = true;
+           
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            label7.Visible = false;
+            textBox2.Visible = false;
+            panel1.Visible = true;
+            label3.Visible = false;
+            label4.Visible = false;
+            label5.Visible = false;
+            label6.Visible = false;
+            label1.Text = "KARE";
+            dikdortgenhesapla.Visible = false;
+            hesapla.Visible = true;
+            ucgenhesaplama.Visible = false;
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            label7.Visible = true;
+            textBox2.Visible = true;
+            panel1.Visible = true;
+            label3.Visible = false;
+            label4.Visible = false;
+            label5.Visible = false;
+            label6.Visible = false;
+            label1.Text = "DİKDÖRTGEN";
+            label2.Text = "KISA KENARI GİRİN:";
+            hesapla.Visible = false;
+            dikdortgenhesapla.Visible = true;
+            ucgenhesaplama.Visible = false;
+        }
+
+        private void ucgenhesaplama_Click(object sender, EventArgs e)
+        {
+            label3.Visible = true;
+            label4.Visible = true;
+            label5.Visible = false;
+            label6.Visible = false;
+          
+            int taban,yukseklik, alanu;
+            taban = Convert.ToInt32(textBox1.Text);
+            yukseklik = Convert.ToInt32(textBox2.Text);
+
+            alanu = (taban * yukseklik)/2;
+     
+
+            label4.Text = alanu.ToString();
+       
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            label3.Visible = true;
+            label4.Visible = true;
+            label5.Visible = true;
+            label6.Visible = true;
+
+            int kisakenar, uzunkenar, aland, cevred;
+            kisakenar = Convert.ToInt32(textBox1.Text);
+            uzunkenar = Convert.ToInt32(textBox2.Text);
+
+            aland = uzunkenar * kisakenar;
+            cevred = 2 * (uzunkenar + kisakenar);
+
+            label4.Text = aland.ToString();
+            label5.Text = cevred.ToString();
+        }
+    }
+}
